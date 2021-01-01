b9e3aa9021bdd9b4c1cbf0124c24af3ea07f417c
David Reid <david@admiredigital.com>
2021-01-01 16:47:16 +0000

    Adds resource loading

--- game.js
+++ game.js
@@ -6,6 +6,13 @@ let player1;
 let player2;
 let ball;
 let highscore = 5;
+let resourcesToLoad = 0;
+let resources = [
+    { id: "player-1", var: window.document.createElement("audio"), file: "assets/player-1.mp3" },
+    { id: "player-2", var: window.document.createElement("audio"), file: "assets/player-2.mp3" },
+    { id: "rebound", var: window.document.createElement("audio"), file: "assets/rebound.mp3" },
+    { id: "score", var: window.document.createElement("audio"), file: "assets/score.mp3" },
+ ];
 
 window.onload = function() {
     console.log('pong onload');
@@ -15,6 +22,7 @@ window.onload = function() {
     ball = new Ball(graphics);
     graphics.setCanvasToPageSize();
     addEventListener('mousemove', mouseMove);
+    loadResources();
 }
 
 window.onresize = function() {
@@ -92,4 +100,30 @@ function gameOver() {
     player2.reset();
 
     clearInterval(loop);
+}
+
+function launchIfReady() {
+    resourcesToLoad--;
+    if (resourcesToLoad == 0) {
+        let startDiv = document.getElementById("start");
+        let gameCanvas = document.getElementById("canvas");
+        let gameOver = document.getElementById("game-over");
+        startDiv.style.display = "block";
+        gameCanvas.style.display = "none";
+        gameOver.style.display = "none";
+    }
+}
+
+function beginLoadingAudio(audioVar, fileName) {
+    audioVar.src = fileName;
+    audioVar.addEventListener('ended', launchIfReady());
+}
+
+function loadResources() {
+    resourcesToLoad = resources.length;
+    for (let i = 0; i < resources.length; i++) {
+        if (resources[i].var != undefined) {
+            this.beginLoadingAudio(resources[i].var, resources[i].file);
+        }
+    }
 }
